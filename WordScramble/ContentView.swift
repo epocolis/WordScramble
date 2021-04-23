7c318ecf34cf0bfa7924ece3bbb51c206e73dcc7
leotis <mekdemcrossit@tutanota.com>
2021-04-23 09:25:25 -0400

    adding list to words

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -5,29 +5,53 @@
 //  Created by Leotis buchanan on 2021-04-23.
 //
 /*
- iOS gives us some really powerful APIs for working with strings, including the ability to split
- them into an array, remove whitespace, and even check spellings.
+ The user interface for this app will be made up of three main SwiftUI views: a NavigationView showing the word they are
+ spelling from, a TextField where they can enter one answer, and a List showing all the words they have entered
+ previously.
 
- In this app, we’re going to be loading a file from our app bundle that contains over 10,000
- eight-letter words, each of which can be used to start the game. These words are stored one per
- line, so what we really want is to split that string up into an array of strings in order that
- we can pick one randomly.
-
- Swift gives us a method called components(separatedBy:) that can converts a single string into
- an array of strings by breaking it up wherever another string is found. For example, this will
- create the array ["a", "b", "c"]:
+ For now, every time users enter a word into the text field, we’ll automatically add it to the list of used words.
+ Later, though, we’ll add some validation to make sure the word hasn’t been used before, can actually be produced from
+ the root word they’ve been given, and is a real word and not just some random letters.
  */
 
 import SwiftUI
 
+
+
+
 struct ContentView: View {
+    @State private var  usedWords  = [String]()
+    @State private var  rootWord  = ""
+    @State private var  newWord   = ""
+    
+    
+    func addNewWord(){
+        let answer = newWord.lowercased().trimmingCharacters(in:.whitespacesAndNewlines)
+        
+        guard answer.count > 0 else {
+            return
+        }
+        
+        usedWords.insert(answer, at:0)
+        newWord = ""
+        
+    }
     
     var body: some View {
-        let input = "a b c"
-        let letters = input.components(separatedBy: " ")
-        
-        
-       return Text("Hello world")
+        NavigationView {
+            VStack {
+                TextField("Enter your word", text: $newWord, onCommit:addNewWord)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .autocapitalization(.none)
+                    .padding()
+
+                List(usedWords, id: \.self) {
+                    Image(systemName: "\($0.count).circle")
+                    Text($0)
+                }
+            }
+            .navigationBarTitle(rootWord)
+        }
     }
 }
 
