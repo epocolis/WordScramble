83a2310eaf2d4699aaac3437b9c96f249d012ab0
leotis <mekdemcrossit@tutanota.com>
2021-04-23 09:45:02 -0400

    running code when our app launches

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -25,6 +25,26 @@ struct ContentView: View {
     @State private var  newWord   = ""
     
     
+    func startGame(){
+        // 1. Find the URL for start.txt in our app bundle
+        if let startWordsURL = Bundle.main.url(forResource:"start", withExtension: "txt") {
+            // 2 . Load start.txt into a string
+            if let startWords = try? String(contentsOf: startWordsURL){
+            // 3 . Split the string up into an array of strings, splitting on line breaks
+                let allWords = startWords.components(separatedBy: "\n")
+                
+                // 4 . Pick one random word, or use "silkworm as a sensible default
+                rootWord = allWords.randomElement() ?? "silkworm"
+                // If we are here everything has worked, so we can exit
+                return
+            }
+            
+        }
+        
+        // If we are here then there was a  problem- trigger a crash and report the error
+        fatalError("Could not load start.txt from bundle")
+    }
+    
     func addNewWord(){
         let answer = newWord.lowercased().trimmingCharacters(in:.whitespacesAndNewlines)
         
@@ -51,6 +71,7 @@ struct ContentView: View {
                 }
             }
             .navigationBarTitle(rootWord)
+            .onAppear(perform:startGame)
         }
     }
 }
